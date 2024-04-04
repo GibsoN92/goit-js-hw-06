88a9ac76b4fac25e15a9214e9cacebf091cbf0c4
GibsoN92 <52374403+GibsoN92@users.noreply.github.com>
2024-04-04 21:59:17 +0200

    v4

--- js/task-09.js
+++ js/task-09.js
@@ -4,12 +4,13 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const widget = document.querySelector(".widget");
-const colorSpan = widget.querySelector(".color");
-const colorButton = widget.querySelector(".change-color");
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const nameColor = document.querySelector(".color");
 
-colorButton.addEventListener("click", function () {
-  const randomColor = getRandomHexColor();
-  document.body.style.backgroundColor = randomColor;
-  colorSpan.textContent = randomColor;
-});
+const changeColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  nameColor.textContent = getRandomHexColor();
+};
+window.onload = (event) => changeColor();
+btn.addEventListener("click", changeColor);
